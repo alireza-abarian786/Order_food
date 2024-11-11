bfaefe36efedad57772b7b5060bf6e72bf791442
alireza-abarian786 <frontdeveloper77@gmail.com>
2024-11-11 22:39:47 +0330

    update responsive

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 let bars = document.querySelector(".fa-bars")
+let closee = document.querySelector(".fa-close")
 let sideMenu = document.querySelector(".top")
+
+
 
 
 bars.addEventListener("click" , function () {
     sideMenu.style.transform = "translateX(0)"
+    sideMenu.style.transition = "2s ease-out 0.2s"
+})
+
+closee.addEventListener("click" , function () {
+    sideMenu.style.transform = "translateX(-250px)"
+    // sideMenu.style.transition = "2s ease-out 0.2s"
 })
 
 
-console.log(bars);
-console.log(sideMenu);
 
 
 
