02f2fa27968b225f5db7cf51150664fe4a2aba39
alireza-abarian786 <frontdeveloper77@gmail.com>
2024-11-11 21:33:16 +0330

    create js file

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let bars = document.querySelector(".fa-bars")
+let sideMenu = document.querySelector(".top")
+
+
+bars.addEventListener("click" , function () {
+    sideMenu.style.transform = "translateX(0)"
+})
+
+
+console.log(bars);
+console.log(sideMenu);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
