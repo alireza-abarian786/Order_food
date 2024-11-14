f3d773d94c8a399515e81844bb18178099a611d1
alireza-abarian786 <frontdeveloper77@gmail.com>
2024-11-14 22:14:27 +0330

    update work part

--- js/script.js
+++ js/script.js
@@ -5,6 +5,14 @@ let sideMenu = document.querySelector(".top")
 let slides = document.getElementsByClassName("slide")
 let currentIndex = 1
 
+let h2Work = document.querySelector(".typo-two-item")
+let after = document.querySelector("h3::after")
+
+
+const box = document.querySelector('.typo-two-item');
+// const style = window.getComputedStyle(box, '::after');
+
+
 function displayImages(n) {
     currentIndex = n    
 
@@ -23,9 +31,12 @@ function displayImages(n) {
 
     slides[currentIndex - 1].style.display = "block";
 }
-
 displayImages(currentIndex)
 
+function changeSlide (n) {    
+    currentIndex += n;
+    displayImages(currentIndex);
+}
 
 bars.addEventListener("click" , function () {
     sideMenu.style.transform = "translateX(0)"
@@ -36,11 +47,13 @@ closee.addEventListener("click" , function () {
     sideMenu.style.transform = "translateX(-250px)"
 })
 
-function changeSlide (n) {    
-    currentIndex += n;
-    displayImages(currentIndex);
-}
+h2Work.addEventListener("mouseover" , function () {    
+    box.classList.add('change-after-width');
+})
 
+h2Work.addEventListener("mouseout" , function () {
+    box.classList.remove('change-after-width');
+})
 
 
 
